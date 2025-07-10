8f2c30f83e90f54f44582651608bd78e09aec564
Dominik Hahn <dominik.hahn@relution.io>
2025-07-10 14:16:17 +0200

    Initial Commit

---
+++ Matcher.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renamer {
+    class Matcher {
+        static string VERSION = "V1.0";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oldName">old pattern</param>
+        /// <param name="newName">new pattern</param>
+        /// <param name="files">List of filenames</param>
+        /// <returns>List of changed filenames</returns>
+        public static List<string> matcher(string oldName, string newName, List<string> files) {
+
+
+            return files;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renamer {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length==0)
+                Console.WriteLine("Bitte Parameter eingeben!");
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine($"Eingabe {i}:{args[i]}");
+            }
+        }
+    }
+
+
+}
